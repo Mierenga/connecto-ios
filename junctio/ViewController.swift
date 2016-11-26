8a5b4cc53f823e2ccf552d4f41da93f0f5e591c6
Mike Swierenga <mike@tomesoftware.com>
2016-11-26 09:48:46 -0500

    Initial Commit

---
+++ junctio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  junctio
+//
+//  Created by Mike Swierenga on 11/26/16.
+//  Copyright © 2016 eskimwier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
